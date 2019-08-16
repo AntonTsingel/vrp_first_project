7b7dd67cdf36d19aa24b74beb29cc90b7f98e0a8
AntonTsynhel <Tsynhel@gmail.com>
2019-08-16 11:29:27 +0300

    HMR with Stylesheets

--- webpack.config.js
+++ webpack.config.js
@@ -3,24 +3,32 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: {
-    app:   './src/index.js'
-},
-    devtool: 'inline-source-map',
-    devServer: {
-      contentBase: './dist',
-      hot: true
-    },   
-    plugins: [
-         new CleanWebpackPlugin(),
-         new HtmlWebpackPlugin({
-           title: 'Output Management'
-         })
-       ],  
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
-    }
+  mode: 'development',
+  entry: {
+    app: './src/index.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  }
 };
