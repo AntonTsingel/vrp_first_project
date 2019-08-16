b6c41c04bb6b8a6da7e2f647672bb53f3efb8a04
AntonTsynhel <Tsynhel@gmail.com>
2019-08-16 14:25:43 +0300

    Dynamic Imports

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ module.exports = {
   mode: 'development',
   entry: {
     index: './src/index.js',
-    another: './src/another-module.js'
   },
   devtool: 'inline-source-map',
   devServer: {
@@ -29,12 +28,8 @@ module.exports = {
   ],
   output: {
     filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/'
   },
-  optimization: {
-    splitChunks: {
-      chunks: 'all'
-    }
-  }
 };
