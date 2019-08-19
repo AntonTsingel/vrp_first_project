0705cb9a10be5dcd60c7fad823c9334a48feec05
AntonTsynhel <Tsynhel@gmail.com>
2019-08-19 19:37:23 +0300

    added task for templates in gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,11 @@ const del = require('del');
 const rigger = require('gulp-rigger');
 const browserSync = require('browser-sync').create();
 
+var handlebars = require('gulp-handlebars');
+var wrap = require('gulp-wrap');
+var declare = require('gulp-declare');
+
+
 
 const cssFiles = [
     './src/components/breadcrumbs/breadcrumbs.css',
@@ -28,35 +33,35 @@ const jsFiles = [
 
 function styles() {
     return gulp.src(cssFiles)
-    
-    .pipe(concat('style.css'))
-    
-    .pipe(autoprefixer({
-        overrideBrowserslist: ['last 2 versions'],
-        cascade: false
-    }))
 
- 
-    .pipe(cleanCSS({
-        level: 2
-    }))
+        .pipe(concat('style.css'))
 
-    .pipe(gulp.dest('./src/dist/css'))
-    .pipe(browserSync.stream());
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions'],
+            cascade: false
+        }))
+
+
+        .pipe(cleanCSS({
+            level: 2
+        }))
+
+        .pipe(gulp.dest('./src/dist/css'))
+        .pipe(browserSync.stream());
 }
 
 function scripts() {
 
     return gulp.src(jsFiles)
 
-    .pipe(concat('script.js'))
+        .pipe(concat('script.js'))
 
-    .pipe(uglify({
-        toplevel: true
-    }))
-    
-    .pipe(gulp.dest('./src/dist/js'))
-    .pipe(browserSync.stream());
+        .pipe(uglify({
+            toplevel: true
+        }))
+
+        .pipe(gulp.dest('./src/dist/js'))
+        .pipe(browserSync.stream());
 
 }
 
@@ -70,15 +75,31 @@ function watch() {
             baseDir: "./src"
         }
     });
-    gulp.watch('./src/components/css/**/*.css', styles)
-    gulp.watch('./src/cpomponents/js/**/*.js', scripts)
-    gulp.watch("./src/**/*.html").on('change', browserSync.reload);
+    gulp.watch('./src/components/**/*.css', styles)
+    gulp.watch('./src/cpomponents/**/*.js', scripts)
+    gulp.watch("./src/**/*.hbs").on('change', browserSync.reload);
 }
+
+async function  temp() {
+    gulp.src('src/components/**/*.html')
+        .pipe(handlebars())
+        .pipe(wrap('Handlebars.template(<%= contents %>)'))
+        .pipe(declare({
+            namespace: 'MyApp.templates',
+            noRedeclare: true, // Avoid duplicate declarations
+        }))
+        .pipe(concat('templates.js'))
+        .pipe(gulp.dest('./src/dist/'));
+        
+}
+
+gulp.task('templates', temp);
+
 
 gulp.task('styles', styles);
 gulp.task('scripts', scripts);
 
 gulp.task('del', clean);
 gulp.task('watch', watch);
-gulp.task('build', gulp.series(clean, gulp.parallel(styles,scripts)));
+gulp.task('build', gulp.series(clean, gulp.parallel(styles, scripts)));
 gulp.task('dev', gulp.series('build', 'watch'));
