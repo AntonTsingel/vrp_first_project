8fe3af00f44328eb6dd7bd4304a469b1de4efe33
AntonTsynhel <Tsynhel@gmail.com>
2019-08-20 18:13:41 +0300

    starting fixing errors

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Output Management'
+      title: 'VRP first project'
     })
   ],
   output: {
