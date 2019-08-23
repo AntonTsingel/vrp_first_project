6fac91d01b667629d6e41ee315bcbe3215b90d88
AntonTsynhel <Tsynhel@gmail.com>
2019-08-23 13:37:35 +0300

    delete unnecessary code

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -51,24 +51,6 @@ var generator = Handlebars.compile(feedback());
 var elem = document.getElementById('feedback'); // output element: DOM Node
 elem.innerHTML = generator();
 
-
-document.getElementById ("feedback").addEventListener("click", show, false);
-
-function show(state)
-{
-  let  i = 1;
-  i++;
-  if (i % 2 == 0) {
-  document.getElementById('feedback').style.display = 'none';
-} else 
-document.getElementById('feedback').style.display = 'block';
-  show(state);
-}
-
-
-
-
-
 var slideIndex = 1;
 showSlides(slideIndex);
 // Next/previous controls
@@ -99,25 +81,3 @@ function importAll(r) {
   }
 
   const images = importAll(require.context('./img', false, /\.(png|jpe?g|svg)$/));
-
-  function component() {
-    const element = document.createElement('div');
-    const button = document.createElement('button');
-    const br = document.createElement('br');
-
-    button.innerHTML = 'Click me and look at the console!';
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.appendChild(br);
-    element.appendChild(button);
-
-    // Note that because a network request is involved, some indication
-    // of loading would need to be shown in a production-level site/app.
-    button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
-        const print = module.default;
-        print();
-    });
-
-    return element;
-}
-
-document.body.appendChild(component());
