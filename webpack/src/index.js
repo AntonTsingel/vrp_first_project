78183e56ce589308eca3b1803fce7c8949550651
AntonTsynhel <Tsynhel@gmail.com>
2019-08-23 14:15:03 +0300

    Fixed slider function.

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -51,29 +51,6 @@ var generator = Handlebars.compile(feedback());
 var elem = document.getElementById('feedback'); // output element: DOM Node
 elem.innerHTML = generator();
 
-var slideIndex = 1;
-showSlides(slideIndex);
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
-}
-console.log('slideFunction');
-
 function importAll(r) {
     let images = {};
     r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
