7b744e9663ba839fc7fd620d50f48932f951686b
AntonTsynhel <Tsynhel@gmail.com>
2019-08-27 13:31:33 +0300

    remove unnecessary code from index.js

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -7,7 +7,7 @@ import './components/info';
 import './components/footer';
 import './common/css/main.css';
 import './components/feedback';
-import Handlebars from 'handlebars/dist/handlebars';
+
 function importAll(r) {
   let images = {};
   r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
@@ -17,20 +17,3 @@ function importAll(r) {
 const images = importAll(require.context('./img', false, /\.(png|jpe?g|svg)$/));
 
 
-/*
-import _ from 'lodash';
-import "./style.css";
-console.log("styles");
-import head from './components/header/header.hbs';
-
-var data = {
-    name: "Название компании",
-    description: "Самая клёвая компания",
-    phone: "+7(499) 777-77-77"
-};
-
-var generator = Handlebars.compile(head(data));
-var elem = document.getElementById('header'); // output element: DOM Node
-elem.innerHTML = generator();
-
-*/
